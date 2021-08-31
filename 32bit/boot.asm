106b1b3b2874c4a64f7378f89f9d58bc7ad30797
Hampton Moore <me@hampton.pw>
2021-08-31 14:46:18 -0400

    32bit mode!

---
+++ 32bit/boot.asm
@@ -0,0 +1,104 @@
+[BITS 16]       ; We need 16-bit intructions for Real mode
+
+[ORG 0x7C00]    ; The BIOS loads the boot sector into memory location 0x7C00
+    cli                     ; Disable interrupts, we want to be alone
+
+    xor ax, ax
+    mov ds, ax              ; Set DS-register to 0 - used by lgdt
+
+    lgdt [gdt_desc]         ; Load the GDT descriptor
+
+    mov eax, cr0            ; Copy the contents of CR0 into EAX
+    or eax, 1               ; Set bit 0
+    mov cr0, eax            ; Copy the contents of EAX into CR0
+
+    jmp 08h:clear_pipe      ; Jump to code segment, offset clear_pipe
+
+
+[BITS 32]                       ; We now need 32-bit instructions
+clear_pipe:
+    mov ax, 10h             ; Save data segment identifyer
+    mov ds, ax              ; Move a valid data segment into the data segment register
+    mov ss, ax              ; Move a valid data segment into the stack segment register
+    mov esp, 090000h        ; Move the stack pointer to 090000h
+
+    push welcome
+    call print
+    jmp hang
+
+printchar:
+    mov eax, [esp+12]
+    imul eax, 80
+    mov edx, [esp + 16]
+    add eax, edx
+    imul eax, 2
+    add eax, 0x0B8000
+    mov  ebx, [esp + 8]
+    mov  ecx, [esp + 4]
+    mov byte [ds:eax], bl
+    mov byte [ds:eax+1], cl
+    ret 16
+
+print:
+    mov  si, [esp + 4]
+    mov ebx, 0
+    mov ecx, 0
+    jmp print_loop
+  
+print_loop:
+    lodsb
+    cmp al, 0
+    je print_exit
+    cmp al, 13
+    je print_newline
+    push ebx   ; save x 
+    push ecx   ; save y 
+    push ebx   ; set x pos
+    push ecx    ; set y pos
+    push eax   ; set character
+    push 0x1B ; set color
+    call printchar
+    pop ecx
+    pop ebx
+    inc ebx
+    cmp ebx, 80
+    je print_newline
+    jmp print_loop
+
+print_newline:
+    inc ecx
+    xor ebx, ebx
+    jmp print_loop
+
+print_exit:
+    ret 8
+
+welcome db "----- Welcome to HamOS -----", 13, "New line bitches", 0
+hang:
+    jmp hang                ; Loop, self-jump
+
+gdt:                    ; Address for the GDT
+gdt_null:               ; Null Segment
+    dd 0
+    dd 0
+gdt_code:               ; Code segment, read/execute, nonconforming
+    dw 0FFFFh
+    dw 0
+    db 0
+    db 10011010b
+    db 11001111b
+    db 0
+gdt_data:               ; Data segment, read/write, expand down
+    dw 0FFFFh
+    dw 0
+    db 0
+    db 10010010b
+    db 11001111b
+    db 0
+gdt_end:                ; Used to calculate the size of the GDT
+gdt_desc:                       ; The GDT descriptor
+    dw gdt_end - gdt - 1    ; Limit (size)
+    dd gdt                  ; Address of the GDT
+
+times 510-($-$$) db 0           ; Fill up the file with zeros
+    dw 0AA55h                ; Boot sector identifyer
